8af4d0de854136c82a0e869f0c53af5c375ed01f
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2022-12-31 12:53:24 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,7 @@
 /*  Terraform to create a single VPC in specified region, using vpc module: 
       - Two subnets, one public and one private 
       - NAT GW, IGW with EIP, routes needed 
-      
-      Use at your own peril, and be mindful of the AWS costs of 
-      building this environment.  
-        -- Dan Edeen, dan@dsblue.net, 2022  --
-	   
+          -- Dan Edeen, dan@dsblue.net, 2022  --   
 */
 #
 # Build VPCs for DataCenters
@@ -21,7 +17,7 @@ module "vpc" {
     azs               = each.value.az_list
     private_subnets   = [each.value.priv_subnet]
     public_subnets    = [each.value.publ_subnet]
-    intra_subnets     = [each.value.intra_subnets]
+    intra_subnets     = [each.value.intra_subnet]
     enable_ipv6             = false
     enable_nat_gateway      = true
     one_nat_gateway_per_az  = false
