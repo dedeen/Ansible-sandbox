a29264897a2f9b4acd7861b01f56c2425b8401ba
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-01-03 21:11:50 -0600

    Update index_html_from_s3.tf

--- index_html_from_s3.tf
+++ index_html_from_s3.tf
@@ -6,30 +6,15 @@
 #	To do next 1/1/23 - redefine the main and per subnet route tables  
 #
 
-
-# VPC parms, can build mutiple by passing in via this map 
-variable "new_vpcs" {
-	description = "DC parms for Oregon VPCs"
-	type		= map(any)
-
-	default = {
-		datacenter1 = {
-			region_dc		= 	"oregon-dc1"
-			cidr			= 	"192.168.0.0/16"
-			az_list			= 	["us-west-2a","us-west-2b"]
-			edge_subnet		= 	"192.168.1.0/24"
-			server_subnet		= 	"192.168.2.0/24" 
-			public_subnet		= 	"192.168.3.0/24"     
-		}  
-/*a		},   
-		datacenter2 = {
-			region_dc		= 	"oregon-dc2"
-			cidr			= 	"192.168.0.0/16"
-			az_list			= 	["us-west-2a","us-west-2b"]
-			publ_subnet		= 	"192.168.7.0/24"
-			priv_subnet		= 	"192.168.8.0/24"       
-			intra_subnet 		= 	"192.168.9.0/24"       
-		}   a*/	
-	}   
+#  Creating an S3 bucket for files that can be retrieved by instances
+resource "aws_s3_bucket" "file-store-for-tf" {
+  bucket = aws_s3_bucket.tf-copy-file-s3.id
+  acl    = "private"
+  tags = {
+    Name = "S3-filestore"
+    Owner = "dan-via-terraform"
+  }
 }
+
+
 ##
