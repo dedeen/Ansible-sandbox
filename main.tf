0ce3664d1d0309c73043708c739230bc23ab67a8
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-01-05 20:53:17 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -269,41 +269,36 @@ resource "aws_security_group" "SG-intra_vpc_v4" {
 #    Also need to open ssh inbound for remote-exec (below), and 
 #    outbound connection for linux to get software updates.  
 	  
-# temp conditional logic to not build under some test runs 
-locals {
-  should_build_ec2 = 0
-}
-
-  resource "aws_instance" "WebSrv1-edge-subnet" {
-    count = local.should_build_ec2 ?1 : 0
-    ami                                 = "ami-094125af156557ca2"
-    instance_type                       = "t2.micro"
-    depends_on 				= [module.vpc,aws_key_pair.generated_key]
-    key_name                            = "${aws_key_pair.generated_key.key_name}"
-    associate_public_ip_address         = true
-    subnet_id                           = module.vpc["datacenter1"].public_subnets[0]	# public == edge
-    vpc_security_group_ids              = [aws_security_group.SG-inbnd_http.id, aws_security_group.SG-inbnd_icmp.id, aws_security_group.SG-allow_ipv4.id]
-    source_dest_check                   = false
-    tags = {
-          Owner = "dan-via-terraform"
-          Name  = "WebSrv1-edge-subnet"
-    }
-    connection {
-            type        	= "ssh"
-            user        	= "ec2-user"
-            timeout     	= "5m"
-            #private_key        = file(local.keypair_name)
-            private_key     	= "${tls_private_key.dev_key.private_key_pem}"
-            host = aws_instance.WebSrv1-edge-subnet.public_ip
-    }
+resource "aws_instance" "WebSrv1-edge-subnet" {
+  count = local.should_build_ec2 ?1 : 0
+  ami                                 = "ami-094125af156557ca2"
+  instance_type                       = "t2.micro"
+  depends_on 				= [module.vpc,aws_key_pair.generated_key]
+  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  associate_public_ip_address         = true
+  subnet_id                           = module.vpc["datacenter1"].public_subnets[0]	# public == edge
+  vpc_security_group_ids              = [aws_security_group.SG-inbnd_http.id, aws_security_group.SG-inbnd_icmp.id, aws_security_group.SG-allow_ipv4.id]
+  source_dest_check                   = false
+  tags = {
+        Owner = "dan-via-terraform"
+        Name  = "WebSrv1-edge-subnet"
+  }
+  connection {
+          type        	= "ssh"
+          user        	= "ec2-user"
+          timeout     	= "5m"
+          #private_key        = file(local.keypair_name)
+          private_key     	= "${tls_private_key.dev_key.private_key_pem}"
+          host = aws_instance.WebSrv1-edge-subnet.public_ip
+  }
             
-   provisioner "remote-exec" {
-           inline = ["sudo yum update -y", 
-                   "sudo amazon-linux-extras install php8.0 mariadb10.5 -y", 
-                   "sudo yum install -y httpd",
-                   "sudo systemctl start httpd",
-                   "sudo systemctl enable httpd"]
-   }   
+ provisioner "remote-exec" {
+         inline = ["sudo yum update -y", 
+                 "sudo amazon-linux-extras install php8.0 mariadb10.5 -y", 
+                 "sudo yum install -y httpd",
+                 "sudo systemctl start httpd",
+                 "sudo systemctl enable httpd"]
+ }   
 }
 
 resource "aws_instance" "BastionHost-edge-subnet" {
