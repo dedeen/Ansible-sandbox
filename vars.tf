08fce252e4bbc6a3561d3f227585b756fc4e88c3
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-06 18:25:47 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -15,15 +15,15 @@ variable "app_vpcs" {
 			az1_subnet		= 	"10.104.0.0/18"
 			az2_subnet		= 	"10.104.64.0/18"
 			 
-		}  
-	 
+		} ; 
+	 	/* }
 		datacenter2 = {
 			region_dc		= 	"App02-VPC"
 			cidr			= 	"10.105.0.0/16"
 			az_list			= 	["us-west-2a","us-west-2b"]
 			az1_subnet		= 	"10.105.0.0/18"
 			az2_subnet		= 	"10.105.64.0/18"
-		}   	
+		}   */	
 	}   
 }
 ##
