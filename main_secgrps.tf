a3ba2de6856b1ad8a448e3d064180e8b3be88a0a
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 13:50:36 -0600

    Update main_secgrps.tf

--- main_secgrps.tf
+++ main_secgrps.tf
@@ -1,27 +1,35 @@
-# Create security groups for each VPC in the project
+# Create security groups for each VPC 
 #
+
+for_each = var.app_vpcs 
+
 #  This secgrp will llow all IPv4 traffic in and out
 resource "aws_security_group" "SG-allow_ipv4" {
-  name                  = "SG-allow_ipv4"
-  description           = "SG-allow_ipv4"
-  depends_on 	        	= [module.vpc["app1vpc"]]
-  vpc_id                = module.vpc["app1vpc"].vpc_id
-  ingress {
-    description         = "inbound v4"
-    cidr_blocks         = ["0.0.0.0/0"]
-    from_port           = 0
-    to_port             = 0
-    protocol            = "-1"
-  }
-  egress {
-    description         = "outbound v4"
-    cidr_blocks         = ["0.0.0.0/0"]
-    from_port           = 0
-    to_port             = 0
-    protocol            = "-1"
-  }
-  tags = {
-    Name = "SG-allow_ipv4"
-    Owner = "dan-via-terraform"
+  for_each = var.app_vpcs 
+    providers = {
+      aws = aws.usw2
+    }
+    name                  = "SG-allow_ipv4"
+    description           = "SG-allow_ipv4"
+    #  depends_on 	        	= each.value. [module.vpc["app1vpc"]]
+    vpc_id                = module.vpc.var.vpc_id
+    ingress {
+      description         = "inbound v4"
+      cidr_blocks         = ["0.0.0.0/0"]
+      from_port           = 0
+      to_port             = 0
+      protocol            = "-1"
+    }
+    egress {
+      description         = "outbound v4"
+      cidr_blocks         = ["0.0.0.0/0"]
+      from_port           = 0
+      to_port             = 0
+      protocol            = "-1"
+    }
+    tags = {
+      Name = "SG-allow_ipv4"
+      Owner = "dan-via-terraform"
+    }
   }
 }
