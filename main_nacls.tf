c72bb5d452f0ef131af8d44d6f11d0d6585deb07
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 16:59:51 -0600

    Update main_nacls.tf

--- main_nacls.tf
+++ main_nacls.tf
@@ -1,11 +1,10 @@
 # Create some base NACLs for each VPC 
 #    NACLs are built per VPC, so iterate through VPCs
 
-
 #  This secgrp will llow all IPv4 traffic in and out
 resource "aws_network_acl" "NACL-allow_ipv4" {
-  for_each = var.app_vpcs 
-    vpc_id                = module.vpc[each.value.map_key].vpc_id
+  for_each      = var.app_vpcs 
+    vpc_id      = module.vpc[each.value.map_key].vpc_id
       
     ingress {
       protocol		= "-1"
@@ -28,3 +27,11 @@ resource "aws_network_acl" "NACL-allow_ipv4" {
       Owner = "dan-via-terraform"
     }
 }
+
+  
+  #  Now that each VPC has one of these ACLs, associate it with the app-VPC instance subnets
+  resource "aws_network_acl_association" "vpc-subnet-NACL-assoc" {
+     network_acl_id   = aws_network_acl.NACL-allow_ipv4["app1vpc"]
+     subnet_id        = module.vpc["app1vpc"].intra_subnets[0]
+  }
+    
