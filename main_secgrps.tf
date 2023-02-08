252f0d39f74f146269c801ad0b7450226e4bc10a
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 14:29:05 -0600

    Update main_secgrps.tf

--- main_secgrps.tf
+++ main_secgrps.tf
@@ -10,7 +10,8 @@ resource "aws_security_group" "SG-allow_ipv4" {
     #  depends_on 	        	= each.value. [module.vpc["app1vpc"]]
     #vpc_id                = module.vpc[2].vpc_id
     #vpc_id                = module.vpc["{var.app_vpcs}"].vpc_id
-    vpc_id                = module.vpc["${each.value}"].vpc_id
+    #vpc_id                = module.vpc["${each.value}"].vpc_id
+    vpc_id                = module.vpc["${var.app_vpcs}"].vpc_id
     ingress {
       description         = "inbound v4"
       cidr_blocks         = ["0.0.0.0/0"]
