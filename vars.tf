61a8d64b9094bba04b27ca1f9d23703756603594
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-07 19:28:52 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -41,7 +41,8 @@ variable "app_vpcs" {
 			cidr			= 	"10.100.0.0/16"
 			az_list			= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2b","us-west-2b","us-west-2b","us-west-2b","us-west-2b","us-west-2b"]
 			vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24","10.100.4.0/24","10.100.5.0/24","10.100.64.0/24","10.100.65.0/24","10.100.66.0/24","10.100.67.0/24","10.100.68.0/24","10.100.69.0/24"]
-			subnet_names	= 	["sec-az1-mgt-snet","sec-az1-int-snet","sec-az1-pub-snet","sec-az2-mgt-snet","sec-az2-int-snet","sec-az2-pub-snet"]
+			subnet_names	= 		["sec-az1-mgt","sec-az1-int","sec-az1-pub","sec-az1-TGW_Att","sec-az1-GWLB_EndPt","sec-az1-GWLB","sec-az2-mgt","sec-az2-int","sec-az2-pub","sec-az2-TGW_Att","sec-az2-GWLB_EndPt","sec-az2-GWLB"]
+		
 		}
 	}   
 }
