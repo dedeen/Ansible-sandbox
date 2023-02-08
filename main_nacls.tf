158557c1e999cce585779c62cefe4595a9c37840
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 16:23:38 -0600

    Update main_nacls.tf

--- main_nacls.tf
+++ main_nacls.tf
@@ -1,33 +1,32 @@
-# Create security groups for each VPC 
-#    SecGrps are per VPC, so iterate through my list of VPC vars and build one SG per VPC
+# Create some base NACLs for each VPC 
+#    NACLs are built per VPC, so iterate through VPCs
 
 
 #  This secgrp will llow all IPv4 traffic in and out
-resource "aws_security_group" "SG-allow_ipv4" {
+resource "aws_network_acl" "NACL-allow_ipv4" {
   for_each = var.app_vpcs 
-    name                  = "SG-allow_ipv4"
-    description           = "SG-allow_ipv4"
-    #  depends_on 	        	= each.value. [module.vpc["app1vpc"]]
-    #vpc_id                = module.vpc[2].vpc_id
-    #vpc_id                = module.vpc["{var.app_vpcs}"].vpc_id
-    #vpc_id                = module.vpc["${each.value}"].vpc_id
+    name                  = "NACL-allow_ipv4"
+    description           = "NACL-allow_ipv4"
     vpc_id                = module.vpc[each.value.map_key].vpc_id
+      
     ingress {
-      description         = "inbound v4"
-      cidr_blocks         = ["0.0.0.0/0"]
-      from_port           = 0
-      to_port             = 0
-      protocol            = "-1"
+      protocol		= "-1"
+      rule_no		= 100
+      action		= "allow"
+      cidr_block		= "0.0.0.0/0"
+      from_port		= 0	# ignored with protocol -1
+      to_port		= 0	# ignored with protocol -1
     }
     egress {
-      description         = "outbound v4"
-      cidr_blocks         = ["0.0.0.0/0"]
-      from_port           = 0
-      to_port             = 0
-      protocol            = "-1"
+      protocol		= "-1"
+      rule_no		= 101
+      action		= "allow"
+      cidr_block		= "0.0.0.0/0"
+      from_port		= 0	# ignored with protocol -1
+      to_port		= 0	# ignored with protocol -1
     }
     tags = {
-      Name = "SG-allow_ipv4"
+      Name = "NACL-allow_ipv4"
       Owner = "dan-via-terraform"
     }
 }
