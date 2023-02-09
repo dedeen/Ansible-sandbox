f26eec012132a649542aa27c9c85f2da03058a75
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 20:27:03 -0600

    Update main_ec2s.tf

--- main_ec2s.tf
+++ main_ec2s.tf
@@ -27,3 +27,31 @@ resource "aws_instance" "EC2-app1vpc-az2" {
           Name  = "EC2-app1vpc-az2"
     }
 }
+
+resource "aws_instance" "EC2-app2vpc-az1" {
+  ami                                 = "ami-094125af156557ca2"
+  instance_type                       = "t2.micro"
+  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  associate_public_ip_address         = false
+  subnet_id                           = module.vpc["app2vpc"].intra_subnets[0]
+  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["app2vpc"].id]  
+  source_dest_check                   = false
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "EC2-app2vpc-az1"
+    }
+}
+  
+resource "aws_instance" "EC2-app2vpc-az2" {
+  ami                                 = "ami-094125af156557ca2"
+  instance_type                       = "t2.micro"
+  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  associate_public_ip_address         = false
+  subnet_id                           = module.vpc["app2vpc"].intra_subnets[2]
+  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["app2vpc"].id]  
+  source_dest_check                   = false
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "EC2-app2vpc-az2"
+    }
+}
