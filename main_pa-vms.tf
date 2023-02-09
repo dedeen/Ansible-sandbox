c1b4ae001aefa0c31fc0b6e84e761a17b1e8b5e6
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 21:14:05 -0600

    Update main_pa-vms.tf

--- main_pa-vms.tf
+++ main_pa-vms.tf
@@ -3,7 +3,7 @@
 
 resource "aws_instance" "PA-VM-1" {
   ami                                 = "ami-094125af156557ca2"
-  instance_type                       = "t2.micro"
+  instance_type                       = "t2.small"                # max 4 NICs
   key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = false
   subnet_id                           = module.vpc["secvpc"].intra_subnets[0]
@@ -13,6 +13,18 @@ resource "aws_instance" "PA-VM-1" {
           Owner = "dan-via-terraform"
           Name  = "PA-VM-1"
     }
+}
+  
+# Add two more NICs to the PA-VM instance 
+resource "aws_network_interface" "eth1" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[1]
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  #private_ips          = ["10.100.1.10"]      
+    
+  attachment  {
+    instance            = PA-VM-1.id
+    device_index        = 1
+  }
 }
   
 
