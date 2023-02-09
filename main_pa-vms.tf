d4b15ebb63a24266c8e484edcd8620221f298ec4
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 21:21:14 -0600

    Update main_pa-vms.tf

--- main_pa-vms.tf
+++ main_pa-vms.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "PA-VM-1" {
   instance_type                       = "t2.small"                # max 4 NICs
   key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = false
-  subnet_id                           = module.vpc["secvpc"].intra_subnets[0]
+  subnet_id                           = module.vpc["secvpc"].intra_subnets[0]           #PA-VM mgmt submet
   vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
   source_dest_check                   = false
   tags = {
@@ -17,7 +17,7 @@ resource "aws_instance" "PA-VM-1" {
   
 # Add two more NICs to the PA-VM instance 
 resource "aws_network_interface" "eth1" {
-  subnet_id             = module.vpc["secvpc"].intra_subnets[1]
+  subnet_id             = module.vpc["secvpc"].intra_subnets[1]                   #internal (app-vpc side) subnet
   security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
   #private_ips          = ["10.100.1.10"]      
     
@@ -26,5 +26,15 @@ resource "aws_network_interface" "eth1" {
     device_index        = 1
   }
 }
-  
+
+resource "aws_network_interface" "eth2" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[2]             #public subnet
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  #private_ips          = ["10.100.1.10"]      
+    
+  attachment  {
+    instance            = aws_instance.PA-VM-1.id
+    device_index        = 2
+  }
+}
 
