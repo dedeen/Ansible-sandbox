7a41fce5971805c7d5e778d64153ca1e334e362e
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 13:37:10 -0600

    Update main_tgw.tf

--- main_tgw.tf
+++ main_tgw.tf
@@ -3,10 +3,10 @@
 resource "aws_ec2_transit_gateway" "TGW-PAN"  {
   description                         = "TGW-PAN"
   amazon_side_asn                     = 64512
-  default_route_table_association     = disable
-  default_route_table_propagation     = disable
-  dns_support                         = enable 
-  vpn_ecmp_support                    = enable 
+  default_route_table_association     = "disable"
+  default_route_table_propagation     = "disable"
+  dns_support                         = "enable" 
+  vpn_ecmp_support                    = "enable"
   
   tags = {
           Owner = "dan-via-terraform"
