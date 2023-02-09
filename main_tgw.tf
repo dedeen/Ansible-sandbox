5523c530df3bd4d4d2895ec312e4fbad1031154e
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 14:10:44 -0600

    Update main_tgw.tf

--- main_tgw.tf
+++ main_tgw.tf
@@ -30,3 +30,21 @@ resource "aws_ec2_transit_gateway_route_table" "TGW-RT-Security-VPC" {
     Name  = "TGW-RT-Security-VPC"
   }
 }
+
+#  Create TGW Attachments in each of the VPCs
+resource "aws_ec2_transit_gateway_vpc_attachment" "app1vpc-att" {
+  subnet_ids              = [module.vpc["app1vpc"].intra_subnets[1],module.vpc["app1vpc"].intra_subnets[3]]
+  transit_gateway_id      = aws_ec2_transit_gateway.TGW-PAN.id
+  vpc_id                  = module.vpc["app1vpc"].vpc_id
+  appliance_mode_support  = "enable"                            # prevents asymm flows between consumer VPC and security VPC
+  dns_support             = "enable"
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Owner = "dan-via-terraform"
+    Name  = "TGW-app1vpc-att"
+  }  
+}
+    
+    
+    
