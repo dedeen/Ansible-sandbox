e71f280757fe64fefdadef3ae44a65206f1d1dba
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 13:32:49 -0600

    Create main_tgw.tf

---
+++ main_tgw.tf
@@ -0,0 +1,15 @@
+#  Terraform to create Transit GW (TGW), attachments in the VPCs, and Route Tables
+
+resource "aws_ec2_transit_gateway" TGW-PAN
+  description                         = "TGW-PAN"
+  amazon_side_asn                     = 64512
+  default_route_table_association     = disable
+  default_route_table_propagation     = disable
+  dns_support                         = enable 
+  vpn_ecmp_support                    = enable 
+  
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "PA-VM-1"
+    }
+}
