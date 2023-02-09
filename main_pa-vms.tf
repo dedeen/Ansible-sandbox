0c1aaa5a5a0528da724da23ca180d67ae8c77850
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 21:56:46 -0600

    Update main_pa-vms.tf

--- main_pa-vms.tf
+++ main_pa-vms.tf
@@ -1,5 +1,5 @@
 #  Terraform to create EC2s as Palo Alto VM-NGFWs
-#    For starters, just building small EC2s with 3 NICs
+#    For starters, just building t2.small EC2s with 3 NICs each, one in each subnet of each AZ
 
 resource "aws_instance" "PA-VM-1" {
   ami                                 = "ami-094125af156557ca2"
@@ -34,6 +34,43 @@ resource "aws_network_interface" "eth2" {
     
   attachment  {
     instance            = aws_instance.PA-VM-1.id
+    device_index        = 2
+  }
+}
+  
+resource "aws_instance" "PA-VM-2" {
+  ami                                 = "ami-094125af156557ca2"
+  instance_type                       = "t2.small"                # max 4 NICs
+  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  associate_public_ip_address         = false
+  subnet_id                           = module.vpc["secvpc"].intra_subnets[6]           #PA-VM mgmt submet
+  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
+  source_dest_check                   = false
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "PA-VM-2"
+    }
+}
+  
+# Add two more NICs to the PA-VM instance 
+resource "aws_network_interface" "eth1" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[7]                   #internal (app-vpc side) subnet
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  #private_ips          = ["10.100.1.10"]      
+    
+  attachment  {
+    instance            = aws_instance.PA-VM-2.id
+    device_index        = 1
+  }
+}
+
+resource "aws_network_interface" "eth2" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[8]             #public subnet
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  #private_ips          = ["10.100.1.10"]      
+    
+  attachment  {
+    instance            = aws_instance.PA-VM-2.id
     device_index        = 2
   }
 }
