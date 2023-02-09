fe7c55077dc20210db7aa8341b7006a08b3c114d
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-08 20:44:03 -0600

    Update main_ec2s.tf

--- main_ec2s.tf
+++ main_ec2s.tf
@@ -5,7 +5,6 @@ resource "aws_instance" "EC2-app1vpc-az1" {
   instance_type                       = "t2.micro"
   key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = false
-  hibernation                         = true
   subnet_id                           = module.vpc["app1vpc"].intra_subnets[0]
   vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["app1vpc"].id]  
   source_dest_check                   = false
