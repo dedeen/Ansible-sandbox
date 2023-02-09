51573ac071e11c06aa7201f956d18585740e4602
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 13:52:42 -0600

    Update main_tgw.tf

--- main_tgw.tf
+++ main_tgw.tf
@@ -1,5 +1,5 @@
 #  Terraform to create Transit GW (TGW), attachments in the VPCs, and Route Tables
-
+#   TGW:
 resource "aws_ec2_transit_gateway" "TGW-PAN"  {
   description                         = "TGW-PAN"
   amazon_side_asn                     = 64512
@@ -7,9 +7,30 @@ resource "aws_ec2_transit_gateway" "TGW-PAN"  {
   default_route_table_propagation     = "disable"
   dns_support                         = "enable" 
   vpn_ecmp_support                    = "enable"
-  
   tags = {
-          Owner = "dan-via-terraform"
-          Name  = "TGW-PAN"
-    }
+    Owner = "dan-via-terraform"
+    Name  = "TGW-PAN"
+  }
+}
+
+#  Route Table for Spoke VPCs
+resource "aws_ec2_transit_gateway_route_table" "TGW-RT-Spoke-VPCs" {
+  transit_gateway_id = aws_ec2_transit_gateway.TGW-PAN.id
+  default_association_route_table     = false
+  default_propagation_route_table     = false
+  tags = {
+    Owner = "dan-via-terraform"
+    Name  = "TGW-RT-Spokes"
+  }
+}
+
+#  Route Table for Security VPC
+resource "aws_ec2_transit_gateway_route_table" "TGW-RT-Security-VPC" {
+  transit_gateway_id = aws_ec2_transit_gateway.TGW-PAN.id
+  default_association_route_table     = false
+  default_propagation_route_table     = false
+  tags = {
+    Owner = "dan-via-terraform"
+    Name  = "TGW-RT-Security-VPC"
+  }
 }
