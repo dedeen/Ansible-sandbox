9c6dcef30b41bb66835fb27d774b703700d5c422
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 23:14:12 -0600

    Update output.tf

--- output.tf
+++ output.tf
@@ -12,10 +12,11 @@ output  "vpc_rtassoc" {
   value = module.vpc["app1vpc"].intra_route_table_association_ids[0]
 }
 
-  
+ /* 
 # Dummy for import cleanup from terraform artifacts
 resource "aws_route_table_association" "temp1" {
   route_table_id  = module.vpc["app1vpc"].intra_route_table_ids[0]
   subnet_id       = module.vpc["app1vpc"].intra_subnets[0]
     
 } 
+*/
