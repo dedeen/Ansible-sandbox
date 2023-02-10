f2ed09fe1fa65f44db27d4576e7b1098d15fcf6c
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 19:04:38 -0600

    Update main_tgw.tf

--- main_tgw.tf
+++ main_tgw.tf
@@ -128,5 +128,11 @@ resource "aws_ec2_transit_gateway_route_table_propagation" "mgmtvpc-to-sec-rt" {
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.TGW-RT-Security-VPC.id
 }
 
-# Create route table for app1vpc instances 
-    dan - start here with app1 route table
+# Create route table for app1vpc instances
+resource "aws_route_table" "app1vpc-rt" {
+  vpc_id                = module.vpc["app1vpc"].vpc_id 
+  route {
+    cidr_block          = 0.0.0.0/0
+    transit_gateway_id  = aws_ec2_transit_gateway.TGW-PAN.id
+  }
+    
