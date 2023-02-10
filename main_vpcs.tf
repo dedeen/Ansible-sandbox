f6a89d03a97744d6b5c74cf0f6c53aefce33c703
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-09 22:48:28 -0600

    Update main_vpcs.tf

--- main_vpcs.tf
+++ main_vpcs.tf
@@ -40,18 +40,3 @@ resource "aws_internet_gateway" "mgmt_vpc_igw" {
 	  Name = "mgmt_vpc_igw"
 	}
 }
-		
-output  "vpc_ID" { 
-  value = module.vpc["app1vpc"].vpc_id
-}
-output  "subnet-0" { 
-  value = module.vpc["app1vpc"].intra_subnets[0]
-}
-output  "module-RT" { 
-  value = module.vpc["app1vpc"].intra_route_table_ids
-}
-output  "vpc_rtassoc" { 
-  value = module.vpc["app1vpc"].intra_route_table_association_ids[0]
-}
-	
-  
