ee993a295ef67bc5a53cfe757477c87d71266d56
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-12 15:01:12 -0600

    Update main_tgw.tf

--- main_tgw.tf
+++ main_tgw.tf
@@ -293,11 +293,11 @@ resource "aws_route_table_association" "sec-pub2-assoc" {
 resource "aws_route_table" "secvpc-rt-private-subnets" {
   vpc_id                = module.vpc["secvpc"].vpc_id 
   route {                                                      
-    cidr_block          = "10.104.0.0/0"                         
+    cidr_block          = "10.104.0.0/24"                         
     transit_gateway_id  = aws_ec2_transit_gateway.TGW-PAN.id
   }
   route {                                                      
-    cidr_block          = "10.105.0.0/16"                       
+    cidr_block          = "10.105.0.0/24"                       
     transit_gateway_id  = aws_ec2_transit_gateway.TGW-PAN.id
   }
   tags = {
