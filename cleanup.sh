90184af7071418ed4d3b534e767462a257d5980f
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-13 00:13:12 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -111,10 +111,10 @@ while [ $index -le 2 ]; do
     
   
     oksofar=true
-    if [ "$currsubnet" != "$subnet1" ]; then 
+    if [ $currsubnet != "$subnet1" ]; then 
     oksofar=false
     fi
-    if [ "$currrtb" != "$rt0" ]; then 
+    if [ $currrtb != "$rt0" ]; then 
     oksofar=false
     fi
     
