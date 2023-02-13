7f269964c754900aa4617f45d0aa3c6bdf89ad43
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-13 14:44:35 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -122,8 +122,8 @@ while [ $index -le $count ]; do
     echo ">"$rtbassoc">"$currrtb">"$currsubnet">"
     echo "<"$subnet1"<"$rt0"<"
     echo "-."${awsrtassoc[$index]}
-    echo "-.."{$awsrtb[$index]}
-    echo {$awssubnet[$index]}
+    echo "-.."${awsrtb[$index]}
+    echo "-..."${awssubnet[$index]}
     #echo "-.-"
     #echo $index">"
     #awsrtassoc[0]=$rtbassoc
