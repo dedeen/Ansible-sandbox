18a35e04cb7c020681ed315144be660712b8ae02
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-13 19:11:46 -0600

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -170,13 +170,7 @@ while [ $index -le $count ]; do
         echo $awsrtcmd
         result2=$(eval "$awsrtcmd")
         echo "... Returned results:"$result2
-        exit 0
     fi
-#rt0=$(aws ec2 describe-route-tables --filters "Name=tag:Name,Values=${orRT}" --query "RouteTables[*].RouteTableId"  --output text)
-#awscmd1="aws ec2 describe-route-tables --route-table-ids ${rt0} --filters \"Name=association.subnet-id,Values=${subnet1}\" --query \"RouteTables[*].Associations[?SubnetId=='${subnet1}']\"  --output text"
-#    result1=$(eval "$awscmd1")
-    
-
 
 index=$(($index+1))
 done
