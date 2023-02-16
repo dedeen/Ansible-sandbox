997d9084e0fd7aede8732b0e85fd92d034e6e623
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-15 22:01:34 -0600

    Update S3_create_ISO_bucket.tf

--- AMI-Builder/S3_create_ISO_bucket.tf
+++ AMI-Builder/S3_create_ISO_bucket.tf
@@ -6,14 +6,17 @@
 #  2. Upload the source OVA into the S3 bucket (may take a while for a big file)
 #       .... 
 #  3. There is a IAM role policy file in the ./AMI-Builder directory named vmimport-trust-policy.json 
-#  3a. Run this command to add the role to your account: 
+#     - Run this command to add the role to your account: 
 #         aws iam create-role --role-name vmimport --assume-role-policy-document file:///..current dir../AMI_Builder/vmimport-trust-policy.json
 #  4. There is another json file in the ./AMI-Builder directory named vmimport-role-policy.json 
-#  4a. Run this command to assign it to the vmimport role created previously
-#         aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document file:///var/tmp/role-policy.json
-#  5. There is a json file in the same ./AMI-Builder directory named vmimport-role-policy.json 
-#  (later)
-
+#     - Run this command to assign it to the vmimport role created previously:
+#         aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document file:///..current dir../AMI_Builder/role-policy.json
+#  5. There is a json file in the same ./AMI-Builder directory named containers.json 
+#     - Run this command to import the OVA from the S3 bucket: It will run for 20 minutes or more. 
+#          aws ec2 import-image --description "DansImport" --license-type BYOL --disk-containers file:///..current dir../AMI_Builder/containers.json
+#     - You can check the run / progress with this command:
+#          aws ec2 describe-import-image-tasks --import-task-ids import-ami-XXXXXXX, where XXXXXX is returned from the previous command. 
+#       Run the command until the status returns as "completed". 
 
 #  Creating an S3 bucket for files to be retrieved by instances
 resource "aws_s3_bucket" "ova-filestore" {
