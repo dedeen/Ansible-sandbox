67646be6ed6aa6f586c8533a1d4c8ab4fbf07b93
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-15 19:11:24 -0600

    Update S3_create_ISO_bucket.tf

--- AMI-Builder/S3_create_ISO_bucket.tf
+++ AMI-Builder/S3_create_ISO_bucket.tf
@@ -3,9 +3,15 @@
 #    - import OVA into the bucket
 #You will need to do the following: 
 #  1. Run this script to create an S3 bucket for the OVA file
-#  2. Copy the source OVA into the S3 bucket
-#  3. 
-#  ....
+#  2. Upload the source OVA into the S3 bucket (may take a while for a big file)
+#       .... 
+#  3. There is a IAM role policy file in the ./AMI-Builder directory named vmimport-trust-policy.json 
+#  3a. Run this command to add the role to your account: 
+#         aws iam create-role --role-name vmimport --assume-role-policy-document file:///..current dir../AMI_Builder/vmimport-trust-policy.json
+#  4. There is another json file in the ./AMI-Builder directory named vmimport-role-policy.json 
+#  4a. Run this command to assign it to the vmimport role created previously
+#         aws iam put-role-policy --role-name vmimport --policy-name vmimport --policy-document file:///var/tmp/role-policy.json
+
 #  (later)
 
 
