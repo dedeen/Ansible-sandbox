81517d7e73ab60636905618ca30b8a2846d476e1
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-15 18:22:51 -0600

    Update S3_create_ISO_bucket.tf

--- AMI-Builder/S3_create_ISO_bucket.tf
+++ AMI-Builder/S3_create_ISO_bucket.tf
@@ -1,6 +1,12 @@
-# Terraform script to build an S3 bucket for ISO to make an AMI for the firewalls:
+# Terraform script to build an S3 bucket for OVA to make an AMI for the firewalls:
 #    - create an S3 bucket 
 #    - import OVA into the bucket
+#You will need to do the following: 
+#  1. Copy this .tf file up a level to the terraform runtime location
+#  2. Copy the source OVA into the .../source_files directory referenced below
+#  3. Run this script
+#  ....
+#  (later)
 
 
 #  Creating an S3 bucket for files to be retrieved by instances
