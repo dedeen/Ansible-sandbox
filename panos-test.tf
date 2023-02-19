cf92d8cf60edbfd8f85854293501babc01d2572e
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-19 16:00:08 -0600

    Update panos-test.tf

--- panos-test.tf
+++ panos-test.tf
@@ -10,42 +10,42 @@ locals {
   aws_linux_ami       = "ami-094125af156557ca2"
   }
 
-resource "aws_instance" "PA-VM-1" {
+resource "aws_instance" "PA-VM-11" {
   #ami                                 = "ami-094125af156557ca2"
   ami                                 = local.panos_ami
   #instance_type                       = "t2.small"                # max 4 NICs
   instance_type                       = "m5.2xlarge"               # PAN min recommendation
   key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = false
-  private_ip                          = "10.100.0.10"
+  private_ip                          = "10.100.0.11"
   subnet_id                           = module.vpc["secvpc"].intra_subnets[0]           #PA-VM mgmt submet
   vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
   source_dest_check                   = false
   tags = {
           Owner = "dan-via-terraform"
-          Name  = "PA-VM-1"
+          Name  = "PA-VM-11"
     }
 }
   
 # Add two more NICs to the PA-VM instance 
-resource "aws_network_interface" "eth1" {
+resource "aws_network_interface" "eth111" {
   subnet_id             = module.vpc["secvpc"].intra_subnets[1]                   #internal (app-vpc side) subnet
   security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
-  private_ips           = ["10.100.1.10"]     
+  private_ips           = ["10.100.1.11"]     
     
   attachment  {
-    instance            = aws_instance.PA-VM-1.id
+    instance            = aws_instance.PA-VM-11.id
     device_index        = 1
   }
 }
 
-resource "aws_network_interface" "eth2" {
+resource "aws_network_interface" "eth211" {
   subnet_id             = module.vpc["secvpc"].intra_subnets[2]             #public subnet
   security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
-  private_ips           = ["10.100.2.10"]      
+  private_ips           = ["10.100.2.11"]      
     
   attachment  {
-    instance            = aws_instance.PA-VM-1.id
+    instance            = aws_instance.PA-VM-11.id
     device_index        = 2
   }
 }
