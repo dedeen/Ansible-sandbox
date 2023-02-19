883b1fe70ad960f1394fbe333626e0db2a141fe8
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-19 15:56:49 -0600

    Create panos-test.tf

---
+++ panos-test.tf
@@ -0,0 +1,51 @@
+#  Terraform to create EC2s as Palo Alto VM-NGFWs
+#    To test all the routes in this environment, run the main_pa-vms_awslinux.tf script. 
+#    To build the PAN-OS version for actual firewalls, run this version (main_pa-vms_panos.tf). 
+#      The main difference is the AMI used to build the EC2 instances. 
+#          Generic AWS linux: "ami-094125af156557ca2"
+#          Palo Alto PA-VM firewall: ami-081f4bfe293d7f414
+
+locals {
+  panos_ami           = "ami-081f4bfe293d7f414"
+  aws_linux_ami       = "ami-094125af156557ca2"
+  }
+
+resource "aws_instance" "PA-VM-1" {
+  #ami                                 = "ami-094125af156557ca2"
+  ami                                 = local.panos_ami
+  #instance_type                       = "t2.small"                # max 4 NICs
+  instance_type                       = "m5.2xlarge"               # PAN min recommendation
+  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  associate_public_ip_address         = false
+  private_ip                          = "10.100.0.10"
+  subnet_id                           = module.vpc["secvpc"].intra_subnets[0]           #PA-VM mgmt submet
+  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
+  source_dest_check                   = false
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "PA-VM-1"
+    }
+}
+  
+# Add two more NICs to the PA-VM instance 
+resource "aws_network_interface" "eth1" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[1]                   #internal (app-vpc side) subnet
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  private_ips           = ["10.100.1.10"]     
+    
+  attachment  {
+    instance            = aws_instance.PA-VM-1.id
+    device_index        = 1
+  }
+}
+
+resource "aws_network_interface" "eth2" {
+  subnet_id             = module.vpc["secvpc"].intra_subnets[2]             #public subnet
+  security_groups       = [aws_security_group.SG-allow_ipv4["secvpc"].id]
+  private_ips           = ["10.100.2.10"]      
+    
+  attachment  {
+    instance            = aws_instance.PA-VM-1.id
+    device_index        = 2
+  }
+}
