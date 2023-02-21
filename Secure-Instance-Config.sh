397e7e4f2323789755d8f4c8a36ba2dd143f88a7
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-20 20:09:22 -0600

    Update Secure-Instance-Config.sh

--- Secure-Instance-Config.sh
+++ Secure-Instance-Config.sh
@@ -19,19 +19,19 @@ echo "VpcId:"${vpcid}
 echo "CIDR:"${cidr}
 
 #Build an IGW so we can access the bastion host from the outside 
-igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
-echo "IGW:"${igwid}
-aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
+#dje>igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
+#dje>echo "IGW:"${igwid}
+#dje>aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
 
 # Attach the bastion IGW to the bastion subnet 
-aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
+#dje>aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
 
 # Get the security group in the target VPC that is wide open for IPv4, name referenced above
 secgroupid=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${open_sec_group} Name=vpc-id,Values=${vpcid} --query "SecurityGroups[*].GroupId" --output text)
 echo "secgrp:"${secgroupid}
 
 # Launch an EC2 that will be a bastion host into the VPC
-aws ec2 run-instances --image-id ${bh_AMI} --instance-type ${bh_type} --subnet-id ${subnetid} --key-name ${bh_keypair} --security-group-ids ${secgroupid} --associate-public-ip-address 
+instid=$(aws ec2 run-instances --image-id ${bh_AMI} --instance-type ${bh_type} --subnet-id ${subnetid} --key-name ${bh_keypair} --security-group-ids ${secgroupid} --associate-public-ip-address --query "Instances[0].InstanceID --output text)
 
 exit 0
 
