7e0b3f3f4db0c353e0d7b017088292205fc1c4b9
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-20 19:34:27 -0600

    Update Secure-Instance-Config.sh

--- Secure-Instance-Config.sh
+++ Secure-Instance-Config.sh
@@ -27,7 +27,7 @@ aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
 #aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
 
 # Get the security group in the target VPC that is wide open for IPv4, named 'SG-allow_ipv4' in this project
-secgroupid=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${open_sec_group} Name=vpc-id,Values=${vpcid} --output text)
+secgroupid=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${open_sec_group} Name=vpc-id,Values=${vpcid} --query "SecurityGroups[*].GroupId" --output text)
 echo "secgrp:"${secgroupid}
 
 # Launch an EC2 that will be a bastion host into the VPC
