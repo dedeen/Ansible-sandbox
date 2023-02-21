2cdd493e01f90730000c10d8a5aa2c4c146b3d8d
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-20 19:35:49 -0600

    Update Secure-Instance-Config.sh

--- Secure-Instance-Config.sh
+++ Secure-Instance-Config.sh
@@ -19,9 +19,9 @@ echo "VpcId:"${vpcid}
 echo "CIDR:"${cidr}
 
 #Build an IGW so we can access the bastion host from the outside 
-igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
-echo "IGW:"${igwid}
-aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
+#igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
+#echo "IGW:"${igwid}
+#aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
 
 # Attach the bastion IGW to the bastion subnet 
 #aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
