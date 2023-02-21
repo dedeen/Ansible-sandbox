60c38be4c9fb395b81548deca4c2f667f06c3cb3
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-20 19:37:20 -0600

    Update Secure-Instance-Config.sh

--- Secure-Instance-Config.sh
+++ Secure-Instance-Config.sh
@@ -19,14 +19,14 @@ echo "VpcId:"${vpcid}
 echo "CIDR:"${cidr}
 
 #Build an IGW so we can access the bastion host from the outside 
-#igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
-#echo "IGW:"${igwid}
-#aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
+igwid=$(aws ec2 create-internet-gateway --query InternetGateway.InternetGatewayId --output text)
+echo "IGW:"${igwid}
+aws ec2 create-tags --resources $igwid --tags Key=Name,Value="Bastion-IGW"
 
 # Attach the bastion IGW to the bastion subnet 
-#aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
+aws ec2 attach-internet-gateway --internet-gateway-id ${igwid} --vpc-id ${vpcid}
 
-# Get the security group in the target VPC that is wide open for IPv4, named 'SG-allow_ipv4' in this project
+# Get the security group in the target VPC that is wide open for IPv4, name referenced above
 secgroupid=$(aws ec2 describe-security-groups --filters Name=group-name,Values=${open_sec_group} Name=vpc-id,Values=${vpcid} --query "SecurityGroups[*].GroupId" --output text)
 echo "secgrp:"${secgroupid}
 
