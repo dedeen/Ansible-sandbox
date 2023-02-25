f06e105850c7c940100be9a1b3d6bdc6d9ee277e
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-24 20:13:44 -0600

    Update Destroy-Bastion-Host-All.sh

--- Destroy-Bastion-Host-All.sh
+++ Destroy-Bastion-Host-All.sh
@@ -2,6 +2,8 @@
 
 # Set up some variables (bh == bastion host)
 debug_flag=1   #0: run straight through script, 1: pause and prompt during script run
+which_bastion_host=1          # 1: build bastion 1 with the local vars listed just below
+#which_bastion_host=2         # 2: build bastion 2 with the local vars listed just below
 
 #Common vars 
 bh_AMI=ami-094125af156557ca2
@@ -9,27 +11,29 @@ bh_type=t2.micro
 bh_keypair=bastion-keypair
 open_sec_group=SG-allow_ipv4
 
-# Var for bastion 1 (App01-VPC)
-bastion_subnet=app1-az1-bastion
-bh_igw_name=Bastion1-IGW
-bh_rt_name=Bastion-Host1-RT
-bh_ec2_name=Bastion-Host1
-bh_vpc_name=App01-VPC-intra         # Actually name of default RT built by terraform
-bh_vpc=App01-VPC                    # Name of the VPC that the bastion host will be created in
+if [ $which_bastion_host -eq 1 ]
+   then 
+      echo "  --> Tearing Down Bastion host 1"
+      # Var for bastion 1 (App01-VPC)
+      bastion_subnet=app1-az1-bastion
+      bh_igw_name=Bastion1-IGW
+      bh_rt_name=Bastion-Host1-RT
+      bh_ec2_name=Bastion-Host1
+      bh_vpc_name=App01-VPC-intra         # Actually name of default RT built by terraform
+      bh_vpc=App01-VPC                    # Name of the VPC that the bastion host will be created in
+  fi
 
-# Var for bastion 2 (App02-VPC)
-#bastion_subnet=app2-az1-bastion
-#bh_igw_name=Bastion2-IGW
-#bh_rt_name=Bastion-Host2-RT
-#bh_ec2_name=Bastion-Host2
-#bh_vpc_name=App02-VPC-intra        # Actually name of default RT built by terraform
-#bh_vpc=App02-VPC                   # Name of the VPC that the bastion host will be created in
-
-#>>instname=Bastion-Host
-#>>igwname=Bastion-IGW
-#>>vpcname=App01-VPC
-#>>bh_rt_name=Bastion-Host-RT
-#>>bastion_subnet=app1-az1-bastion
+if [ $which_bastion_host -eq 2 ]
+   then 
+      echo "  --> Tearing Down Bastion host 2"
+      # Var for bastion 2 (App02-VPC)
+      bastion_subnet=app2-az1-bastion
+      bh_igw_name=Bastion2-IGW
+      bh_rt_name=Bastion-Host2-RT
+      bh_ec2_name=Bastion-Host2
+      bh_vpc_name=App02-VPC-intra        # Actually name of default RT built by terraform
+      bh_vpc=App02-VPC                   # Name of the VPC that the bastion host will be created in
+  fi 
 
 # Terminate the EC2 bastion host
 #   Filtering on only running instances, in case you run tihs multiple times the previously terminate EC2s will be returned by cmd for a while
