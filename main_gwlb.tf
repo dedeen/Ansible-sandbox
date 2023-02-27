23ddb2393864871121bf81150c88077471c1f1c8
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-27 17:46:18 -0600

    Update main_gwlb.tf

--- main_gwlb.tf
+++ main_gwlb.tf
@@ -1,6 +1,30 @@
 #  Terraform to create Gateway Load Balancer for Palo Alto middlebox project
 #
-resource "aws_ec2_transit_gateway" "TGW-PAN"  {
+resource "aws_lb" "PAVMGWLB2" {
+  name                                = "PAVMGWLB2"
+  load_balancer_type                  = "gateway"
+  internal                            = true
+  enable_cross_zone_load_balancing    = true
+  ip_address_type                     = "ipv4"
+  
+  subnet_mapping  {
+    subnet_id                         = module.vpc["secvpc"].intra_subnets[5]
+  }
+  subnet_mapping  {
+    subnet_id                         = module.vpc["secvpc"].intra_subnets[11]
+  }
+  tags = {
+    Owner = "dan-via-terraform"
+    Name  = "TGW-RT-Spokes"
+  }
+}    
+
+    
+    
+    
+    
+    
+    resource "aws_ec2_transit_gateway" "TGW-PAN"  {
   description                         = "TGW-PAN"
   amazon_side_asn                     = 64512
   default_route_table_association     = "disable"
