9476b58905a5e9d4d2841b48fc2bd8a599ec94af
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-26 18:11:12 -0600

    Update utility.sh

--- utility.sh
+++ utility.sh
@@ -1,9 +1,9 @@
-#### Simple script to rename ec2 building .tf files from .tf.not to .tf so they can be run. 
-#      This is used primarily to build and tear down infrastructure without building the EC2s (minimize costs for routing / subnetting, TGW, work. 
+#### Simple script to rename ec2 building name.tf files from .tf.not to .tf so they can be run. Running this script and 
+#        the subsequent 'terraform -apply' will create numerous EC2 instances in your environment. 
 #
-# Here we set up a list of the subnet associations that will need to be changed after all of the terraform scripts have been run. 
-#       Using 4 arrays here that are matched in order on index
-
 mv main_ec2s.tf.not main_ec2s.tf
-mv main_pa-vms.tf.not main_pa-vms.tf
+mv main_pa-vm1.tf.not main_pa-vm1.tf
+mv main_pa_vm2.tf.not main_pa_vm2.tf
+mv main_pa-vm1.tf.not main_pa-vms.tf
 mv main_panorama.tf.not main_panorama.tf
+mv main_webservers.tf.not main_webservers.tf
