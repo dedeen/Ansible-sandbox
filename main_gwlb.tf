8b1440fd3757b5520efe941065a0990c1854f39a
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-27 21:49:52 -0600

    Update main_gwlb.tf

--- main_gwlb.tf
+++ main_gwlb.tf
@@ -41,4 +41,16 @@ resource "aws_lb_target_group" "PAVMTargetGroup2" {
     Name  = "PAVM_GWLB_TG2"
   }
 }
-  
+ 
+resource "aws_lb_listener" "lb_listener1" {
+  load_balancer_arn   = aws_lb.PAVMGWLB2.id
+  port                = "6081"
+  protocol            = "GENEVE"
+  default_action {
+    target_group_arn  = aws_lb_target_group.PAVMTargetGroup2.id
+    type              = "forward"
+  }
+}
+    
+    
+    
