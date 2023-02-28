bf5135eb57655cdbcf6934f72b8e71c3d003efe2
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-02-27 19:35:10 -0600

    Update main_gwlb.tf

--- main_gwlb.tf
+++ main_gwlb.tf
@@ -1,5 +1,12 @@
 #  Terraform to create Gateway Load Balancer for Palo Alto middlebox project
 #
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/awb"
+    }
+  }
+}
 resource "aws_lb" "PAVMGWLB2" {
   name                                = "PAVMGWLB2"
   load_balancer_type                  = "gateway"
