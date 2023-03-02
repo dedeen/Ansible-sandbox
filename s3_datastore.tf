16b8d98ad5cd0aadc770997bd53ef124cca7f3dc
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-02 15:04:28 -0600

    Update s3_datastore.tf

--- s3_datastore.tf
+++ s3_datastore.tf
@@ -22,28 +22,28 @@ resource "aws_s3_object" "init_config" {
   bucket                  = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
       #key                   = "${each.value.fw_name}${pan_config_dir}"
-      key                   = "${each.value.fw_name}${each.value.init_file_key}"
+      key                   = "${each.value.fw_name}/${each.value.config_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_content" {
   bucket                  = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
-      key                   = "${var.fw_name}/${pan_content_dir}"
+      key                   = "${var.fw_name}/${each.value.content_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_license" {
   bucket                  = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
-      key                   = "${var.fw_name}/${pan_license_dir}"
+      key                   = "${var.fw_name}/${each.value.license_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_software" {
   bucket                  = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
-      key                   = "${var.fw_name}/${pan_software_dir}"
+      key                   = "${var.fw_name}/${each.value.software_dir}"
       content               = "application/x-directory"
 }
 
