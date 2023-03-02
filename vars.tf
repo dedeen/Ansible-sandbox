41124f0d32eb68a4f1fa0cc2d3d7d8cebf835527
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-02 14:40:45 -0600

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -62,7 +62,12 @@ variable "app_vpcs" {
 	}   
 }
 
-##
+#Palo Alto Firewall Bootstrap Directories - required subdirs 
+variable pan_config_dir      			= "config/"
+variable pan_content_dir     			= "content/"
+variable pan_license_dir     			= "license/"
+variable pan_software_dir    			= "software/"
+
 variable "pavm_firewalls" {
 	description = "Firewall Parameters"
 	type		= map(any)
