5573db8849049094b1d467fbf5d071b895c70665
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-02 14:39:38 -0600

    Update s3_datastore.tf

--- s3_datastore.tf
+++ s3_datastore.tf
@@ -47,26 +47,25 @@ resource "aws_s3_object" "init_software" {
 }
 
 
-/*
-resource "aws_s3_object" "init_cfg" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
-    for_each                = var.pavm_firewalls 
-      key                   = each.value.init_file_key
-      source                = each.value.init_file
-      source_hash           = filemd5(each.value.init_file)
-      etag                  = filemd5(each.value.init_file) # checked on each tf apply and will replace file if changed
-      force_destroy         = true     
-}
+#resource "aws_s3_object" "init_cfg" {
+#  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+#    for_each                = var.pavm_firewalls 
+#      key                   = each.value.init_file_key
+#      source                = each.value.init_file
+#      source_hash           = filemd5(each.value.init_file)
+#      etag                  = filemd5(each.value.init_file) # checked on each tf apply and will replace file if changed
+#      force_destroy         = true     
+#}
 
-resource "aws_s3_object" "bootstrap_xml" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
-    for_each                = var.pavm_firewalls 
-      key                   = each.value.bootstrap_file_key
-      source                = each.value.bootstrap_file
-      source_hash           = filemd5(each.value.bootstrap_file)
-      etag                  = filemd5(each.value.bootstrap_file) # checked on each tf apply and will replace file if changed
-      force_destroy         = true     
-}
+#resource "aws_s3_object" "bootstrap_xml" {
+#  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+#    for_each                = var.pavm_firewalls 
+#      key                   = each.value.bootstrap_file_key
+#      source                = each.value.bootstrap_file
+#      source_hash           = filemd5(each.value.bootstrap_file)
+#      etag                  = filemd5(each.value.bootstrap_file) # checked on each tf apply and will replace file if changed
+#      force_destroy         = true     
+#}
 
 
 
