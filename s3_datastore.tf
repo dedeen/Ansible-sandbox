d072ede0779b48aff7a930fb2bd080aea5a6eb2f
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-02 19:11:02 -0600

    Update s3_datastore.tf

--- s3_datastore.tf
+++ s3_datastore.tf
@@ -15,48 +15,45 @@ resource "aws_s3_bucket_acl" "acl-pavm-s3-ds" {
   acl    = "private"
  }
 
-## Copy bootstrap files to the S3 bucket, firewalls will load from there
-
-# Set up required bootstrap directory structure 
+# Set up required bootstrap directory structure: /config, /content, /license, /software are mandatory dirs for PAN bootstrap to function 
 resource "aws_s3_object" "init_config" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+  bucket                    = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
-      #key                   = "${each.value.fw_name}${pan_config_dir}"
       key                   = "${each.value.fw_name}/${each.value.config_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_content" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+  bucket                    = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
       key                   = "${each.value.fw_name}/${each.value.content_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_license" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+  bucket                    = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
       key                   = "${each.value.fw_name}/${each.value.license_dir}"
       content               = "application/x-directory"
 }
 
 resource "aws_s3_object" "init_software" {
-  bucket                  = aws_s3_bucket.pavm-s3-ds.id
+  bucket                    = aws_s3_bucket.pavm-s3-ds.id
     for_each                = var.pavm_firewalls 
       key                   = "${each.value.fw_name}/${each.value.software_dir}"
       content               = "application/x-directory"
 }
 
-
-#resource "aws_s3_object" "init_cfg" {
-#  bucket                  = aws_s3_bucket.pavm-s3-ds.id
-#    for_each                = var.pavm_firewalls 
-#      key                   = each.value.init_file_key
-#      source                = each.value.init_file
+# Copy the (2) bootstrap files to the S3 bucket, and firewalls will use to bootstrap their configs
+resource "aws_s3_object" "init_cfg_txt" {
+  bucket                    = aws_s3_bucket.pavm-s3-ds.id
+    for_each                = var.pavm_firewalls 
+      key                   = "/${each.value.fw_name}/${each.value.config_dir}${each.value.init_file_key}"
+      source                = "${each.value.init_file}"
 #      source_hash           = filemd5(each.value.init_file)
 #      etag                  = filemd5(each.value.init_file) # checked on each tf apply and will replace file if changed
 #      force_destroy         = true     
-#}
+}
 
 #resource "aws_s3_object" "bootstrap_xml" {
 #  bucket                  = aws_s3_bucket.pavm-s3-ds.id
