7adfc6eae03d0cba043e2ecb6bba0b00e56a6c5b
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-07 14:36:02 -0600

    Update Configure-Panorama.sh

--- Configure-Panorama.sh
+++ Configure-Panorama.sh
@@ -3,6 +3,7 @@
 pano1_inst_name=Panorama-1
 pano2_inst_name=Panorama-2
 
+cmd99=exit
 
 # Get the handle for Panorama
 instid=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${pano1_inst_name} "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
@@ -25,7 +26,12 @@ echo "|   ... Sending config commands  "
 # ssh to server and configure it 
 login_string="ssh admin@"${publicip}" -i "${ws_keypair}".pem -o StrictHostKeyChecking=accept-new"
 echo ">>"${login_string}
-#ssh admin@${publicip} -i
+
+result1=$(eval "$login_string")
+echo $result1
+
+result99=$$(eval "$cmd99")
+echo $result99
 
 
 #echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
