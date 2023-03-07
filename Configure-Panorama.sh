67fde1110e9b0f9e20741303e2e94d7490e3dd56
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-07 14:15:12 -0600

    Update Configure-Panorama.sh

--- Configure-Panorama.sh
+++ Configure-Panorama.sh
@@ -6,18 +6,20 @@ pano2_inst_name=Panorama-2
 
 # Get the handle for Panorama
 instid=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${pano1_inst_name} "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
-echo "Panorama Server:"${pano1_inst_name}", InstanceID:"${instid}
+#echo "Panorama Server:"${pano1_inst_name}", InstanceID:"${instid}
 
 # Get the keypair name as we will use in config script to connect via ssh
 ws_keypair=$(aws ec2 describe-instances --instance-ids ${instid} --query "Reservations[*].Instances[*].KeyName" --output text)
-echo "Instance KeyPair:"${ws_keypair}
+#echo "Instance KeyPair:"${ws_keypair}
 
 # Get the dyn public IP address 
 publicip=$(aws ec2 describe-instances --instance-ids ${instid} --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
 
 echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "|   Panorama: "${pano1_inst_name} 
-echo "|   PublicIP :"${publicip}
+echo "!     InstanceId : "${instid}
+echo "|     PublicIP   : "${publicip}
+echo "|     KeyPair    : "{ws_keypair}
 echo "|   ... Sending config commands  "
 echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
