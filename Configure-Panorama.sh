f0d9575a567983ba4de39ae2730f3a9ea7497e7b
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-07 18:43:36 -0600

    Update Configure-Panorama.sh

--- Configure-Panorama.sh
+++ Configure-Panorama.sh
@@ -7,34 +7,35 @@ pano2_inst_name=Panorama-2
 cmd99=exit
 
 # Get the handle for Panorama
-instid=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${pano1_inst_name} "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
-#echo "Panorama Server:"${pano1_inst_name}", InstanceID:"${instid}
-
+instid1=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${pano1_inst_name} "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
 # Get the keypair name as we will use in config script to connect via ssh
-ws_keypair=$(aws ec2 describe-instances --instance-ids ${instid} --query "Reservations[*].Instances[*].KeyName" --output text)
-#echo "Instance KeyPair:"${ws_keypair}
-
+ws_keypair1=$(aws ec2 describe-instances --instance-ids ${instid1} --query "Reservations[*].Instances[*].KeyName" --output text)
 # Get the dyn public IP address 
-publicip=$(aws ec2 describe-instances --instance-ids ${instid} --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+publicip1=$(aws ec2 describe-instances --instance-ids ${instid1} --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
+
+# Get the handle for Panorama
+instid2=$(aws ec2 describe-instances --filters Name=tag:Name,Values=${pano2_inst_name} "Name=instance-state-name,Values=running" --query "Reservations[*].Instances[*].InstanceId" --output text)
+# Get the keypair name as we will use in config script to connect via ssh
+ws_keypair2=$(aws ec2 describe-instances --instance-ids ${instid2} --query "Reservations[*].Instances[*].KeyName" --output text)
+# Get the dyn public IP address 
+publicip2=$(aws ec2 describe-instances --instance-ids ${instid2} --query "Reservations[*].Instances[*].PublicIpAddress" --output text)
 
 echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo "|   Panorama: "${pano1_inst_name} 
-echo "!     InstanceId : "${instid}
-echo "|     PublicIP   : "${publicip}
-echo "|     KeyPair    : "${ws_keypair}
-echo "|   ... Sending config commands  "
+echo "!     InstanceId : "${instid1}
+echo "|     PublicIP   : "${publicip1}
+echo "|     KeyPair    : "${ws_keypair1}
+echo "|  "
+echo "|   Panorama: "${pano2_inst_name} 
+echo "!     InstanceId : "${instid2}
+echo "|     PublicIP   : "${publicip2}
+echo "|     KeyPair    : "${ws_keypair2}
+echo "|  "
 
 # ssh to server and configure it 
 #login_string="ssh admin@"${publicip}" -i "${ws_keypair}".pem -o StrictHostKeyChecking=accept-new"
 login_string="ssh admin@"${publicip}" -i "${ws_keypair}".pem -o StrictHostKeyChecking=no"
 echo ">>"${login_string}
-
-result1=$(eval "$login_string")
-echo $result1
-
-result99=$$(eval "$cmd99")
-echo $result99
-
 
 #echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 
