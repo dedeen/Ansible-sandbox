061b2b5e8ee0add19549f9f0009ba491121fd1f6
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-09 01:57:51 -0600

    Update getmetadata.sh

--- Webservers/getmetadata.sh
+++ Webservers/getmetadata.sh
@@ -7,22 +7,23 @@
 meta_data_file=/var/www/html/instance_meta_data.txt
 sudo touch "${meta_data_file}"
 sudo chmod 777 "${meta_data_file}"
-
+#
 echo ". . . . . . . . . . . . " >> "${meta_data_file}"
 echo "Instance Meta-Data" >> "${meta_data_file}"
 echo -n "Private IP:" >> "${meta_data_file}"
 sudo curl http://169.254.169.254/latest/meta-data/local-ipv4 >> "${meta_data_file}"
 echo "" >> "${meta_data_file}"
-
+#
 echo -n "Public IP:" >> "${meta_data_file}"
 sudo curl http://169.254.169.254/latest/meta-data/public-ipv4 >> "${meta_data_file}"
 echo "" >> "${meta_data_file}"
-
+#
 echo -n "Instance ID:" >> "${meta_data_file}"
 sudo curl http://169.254.169.254/latest/meta-data/instance-id >> "${meta_data_file}"
 echo "" >> "${meta_data_file}"
-
+#
 echo -n "Instance Type:" >> "${meta_data_file}"
 sudo curl http://169.254.169.254/latest/meta-data/instance-type >> "${meta_data_file}"
 echo "" "${meta_data_file}"
 echo ". . . . . . . . . . . . " >> "${meta_data_file}"
+#
