f49c867ba4ce548115f91d6aa731f4e971b24b63
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-15 10:01:12 -0500

    Changed end user VPC tags

--- Create-Bastion-Host-All.sh
+++ Create-Bastion-Host-All.sh
@@ -1,4 +1,4 @@
-#### This script will build an EC2 bastion host in a preexisting subnet in the App endpoint VPC. 
+#### This script will build an EC2 bastion host in a preexisting subnet in the user endpoint VPC. 
 #      This will allow us to SSH in from the outside, then ssh to the other EC2s to set up traffic out via the Palo Alto Firewalls. 
 #      This is expected to be a temporary setup, and will use a separate AWS keypair for the bastion EC2. The keypair is in this 
 #        same directory. 
@@ -17,25 +17,25 @@ open_sec_group=SG-allow_ipv4
 if [ $which_bastion_host -eq 1 ]
    then 
       echo "  --> Setting up to build Bastion host 1"
-      # Var for bastion 1 (App01-VPC)
-      bastion_subnet=app1-az1-bastion
+      # Var for bastion 1 (Usr01-VPC)
+      bastion_subnet=usr1-az1-bastion
       bh_igw_name=Bastion1-IGW
       bh_rt_name=Bastion-Host1-RT
       bh_ec2_name=Bastion-Host1
-      bh_vpc_name=App01-VPC-intra         # Actually name of default RT built by terraform
-      bh_vpc=App01-VPC                    # Name of the VPC that the bastion host will be created in
+      bh_vpc_name=Usr01-VPC-intra         # Actually name of default RT built by terraform
+      bh_vpc=Usr01-VPC                    # Name of the VPC that the bastion host will be created in
   fi
 
 if [ $which_bastion_host -eq 2 ]
    then 
       echo "  --> Setting up to build Bastion host 2"
-      # Var for bastion 2 (App02-VPC)
-      bastion_subnet=app2-az1-bastion
+      # Var for bastion 2 (Usr02-VPC)
+      bastion_subnet=usr2-az1-bastion
       bh_igw_name=Bastion2-IGW
       bh_rt_name=Bastion-Host2-RT
       bh_ec2_name=Bastion-Host2
-      bh_vpc_name=App02-VPC-intra        # Actually name of default RT built by terraform
-      bh_vpc=App02-VPC                   # Name of the VPC that the bastion host will be created in
+      bh_vpc_name=Usr02-VPC-intra        # Actually name of default RT built by terraform
+      bh_vpc=Usr02-VPC                   # Name of the VPC that the bastion host will be created in
   fi 
 
 # Get some info from AWS for the target subnet
