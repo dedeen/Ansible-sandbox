718d208ea6a8f55e5b959d70562c765dad02f229
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-15 10:45:29 -0500

    Changed end user VPC tags

--- main_secgrps.tf
+++ main_secgrps.tf
@@ -2,15 +2,11 @@
 #    SecGrps are per VPC, so iterate through my list of VPC vars and build one SG per VPC
 
 
-#  This secgrp will llow all IPv4 traffic in and out
+#  This secgrp will allow all IPv4 traffic in and out
 resource "aws_security_group" "SG-allow_ipv4" {
   for_each = var.app_vpcs 
     name                  = "SG-allow_ipv4"
     description           = "SG-allow_ipv4"
-    #  depends_on 	        	= each.value. [module.vpc["app1vpc"]]
-    #vpc_id                = module.vpc[2].vpc_id
-    #vpc_id                = module.vpc["{var.app_vpcs}"].vpc_id
-    #vpc_id                = module.vpc["${each.value}"].vpc_id
     vpc_id                = module.vpc[each.value.map_key].vpc_id
     ingress {
       description         = "inbound v4"
