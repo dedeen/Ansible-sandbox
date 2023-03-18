199bf20b86872a8ebbbb7aa5ccfc75a522c71b36
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-18 02:20:51 -0500

    Create getmetadata_white.sh

---
+++ Webservers/scripts/getmetadata_white.sh
@@ -0,0 +1,23 @@
+filename=/var/www/html/instance_meta_data.html
+sudo touch "${filename}"
+sudo chmod 755 "${filename}"
+echo "<body>" >> "${filename}"
+echo "<font color=white>" >> "${filename}"
+echo "===============================" >> "${filename}"
+echo "    Instance MetaData<br>" >> "${filename}"
+echo -n "Private IP:    " >> "${filename}"
+sudo curl http://169.254.169.254/latest/meta-data/local-ipv4 >> "${filename}"
+echo "<br>" >> "${filename}"
+#
+echo -n "Instance Id:   " >> "${filename}"
+sudo curl http://169.254.169.254/latest/meta-data/instance-id >> "${filename}"
+echo "<br>" >> "${filename}"
+#
+echo -n "Instance Type: " >> "${filename}"
+sudo curl http://169.254.169.254/latest/meta-data/instance-type >> "${filename}"
+echo "<br>" >> "${filename}"
+#
+echo -n "Avail Zone:    " >> "${filename}"
+sudo curl http://169.254.169.254/latest/meta-data/placement/availability-zone >> "${filename}"
+echo "<br>" >> "${filename}"
+echo "===============================" >> "${filename}"
