c82903eeecef0eb1480b5638dc7f0f0440b664df
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-03-22 21:23:15 -0500

    Update splunk1_cfg.sh

--- Webservers/splunk-scripts/splunk1_cfg.sh
+++ Webservers/splunk-scripts/splunk1_cfg.sh
@@ -1,10 +1,17 @@
 # initial configuration for Splunk server(s)
 # copied from S3 to the splunk instances and executed upon launch
 #
+origpwdfilename=/opt/splunk/etc/passwd 
+bypasspwdfilename=/opt/splunk/etc/passwd.bk
+sudo chmod 777 "${origpwdfilename}"
+sudo touch "${bypasspwdfilename}"
+sudo chmod 777 "${bypasspwdfilename}"
+sudo mv /opt/splunk/etc/passwd /opt/splunk/etc/passwd.bk
+
 seedfilename=/opt/splunk/etc/system/local/user-seed.conf
 inputsfilename=/opt/splunk/etc/system/local/inputs.conf
 #
-sudo mv /opt/splunk/etc/passwd /opt/splunk/etc/passwd.bk
+#sudo mv /opt/splunk/etc/passwd /opt/splunk/etc/passwd.bk
 #
 sudo touch "${seedfilename}"
 sudo chmod 755 "${seedfilename}"
