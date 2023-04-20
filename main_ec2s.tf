81856f788df3aee6a36631d5e24f6a50b40df129
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-04-19 22:04:00 -0500

    Update main_ec2s.tf

--- main_ec2s.tf
+++ main_ec2s.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "secvpc-az1-linux" {
   key_name                            = "bastion-keypair"
   #key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = true
-  private_ip                          = "10.100.3.10"
+  private_ip                          = "10.100.3.20"
   subnet_id                           = module.vpc["secvpc"].intra_subnets[3]
   vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
   source_dest_check                   = true
@@ -21,9 +21,10 @@ resource "aws_instance" "secvpc-az1-linux" {
 resource "aws_instance" "secvpc-az2-linux" {
   ami                                 = "ami-094125af156557ca2"
   instance_type                       = "t2.micro"
-  key_name                            = "${aws_key_pair.generated_key.key_name}"
+  key_name                            = "bastion-keypair"
+  #key_name                            = "${aws_key_pair.generated_key.key_name}"
   associate_public_ip_address         = true
-  private_ip                          = "10.100.67.10"
+  private_ip                          = "10.100.67.20"
   subnet_id                           = module.vpc["secvpc"].intra_subnets[7]
   vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
   source_dest_check                   = true
@@ -33,3 +34,32 @@ resource "aws_instance" "secvpc-az2-linux" {
     }
 }
 ##
+
+# First Cisco ASAv firewall 
+locals {
+  asav_ami           = "ami-0e59c968be56bcc4d"  # BYOL AMI, ASAv version 9.19.1 
+  asav_inst_type     = "c5.xlarge"               # Cisco min recommendation
+  }  
+
+resource "aws_instance" "ASAv-1" {
+  ami                                 = local.asav_ami
+  instance_type                       = local.asav_inst_type           
+  #key_name                            = "${aws_key_pair.generated_key.key_name}"
+  key_name                            = "bastion-keypair"
+  associate_public_ip_address         = false
+  private_ip                          = "10.100.3.10"
+  subnet_id                           = module.vpc["secvpc"].intra_subnets[3]           #PA-VM mgmt submet
+  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
+  source_dest_check                   = false
+  #iam_instance_profile                = aws_iam_instance_profile.ec2_profile.id   # Allow this instance to get bootstrap config from S3 bucket
+  user_data = <<EOF
+vmseries-bootstrap-aws-s3bucket=pavm-s3-ds/PA-VM-1
+EOF
+
+  tags = {
+          Owner = "dan-via-terraform"
+          Name  = "ASAv-1"
+    }
+}
+  
+  
