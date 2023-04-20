c1f5529f6c8b21d40670457a8f25956a2284df6a
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-04-19 23:26:40 -0500

    Update main_asav.tf

--- main_asav.tf
+++ main_asav.tf
@@ -1,39 +1,4 @@
-#  Terraform to create end user EC2s 
-
-#  First EC2 in VPC=secvpc, subnet=sec-az1-mgmt, vars.tf subnet index = 3
-resource "aws_instance" "secvpc-az1-linux" {
-  ami                                 = "ami-094125af156557ca2"
-  instance_type                       = "t2.micro"
-  key_name                            = "bastion-keypair"
-  #key_name                            = "${aws_key_pair.generated_key.key_name}"
-  associate_public_ip_address         = true
-  private_ip                          = "10.100.3.20"
-  subnet_id                           = module.vpc["secvpc"].intra_subnets[3]
-  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
-  source_dest_check                   = true
-  tags = {
-          Owner = "dan-via-terraform"
-          Name  = "secvpc-az1-linux"
-    }
-}
- 
-#  Second end user EC2 in VPC=usr1vpc, subnet=usr1-az2-inst, vars.tf subnet index = 7
-resource "aws_instance" "secvpc-az2-linux" {
-  ami                                 = "ami-094125af156557ca2"
-  instance_type                       = "t2.micro"
-  key_name                            = "bastion-keypair"
-  #key_name                            = "${aws_key_pair.generated_key.key_name}"
-  associate_public_ip_address         = true
-  private_ip                          = "10.100.67.20"
-  subnet_id                           = module.vpc["secvpc"].intra_subnets[7]
-  vpc_security_group_ids              = [aws_security_group.SG-allow_ipv4["secvpc"].id]  
-  source_dest_check                   = true
-  tags = {
-          Owner = "dan-via-terraform"
-          Name  = "secvpc-az2-linux"
-    }
-}
-##
+#  Terraform to create ASAvs on AWS
 
 # First Cisco ASAv firewall 
 #     Building these firewalls with 4 interfaces: mgmt, public, private, and dmz
