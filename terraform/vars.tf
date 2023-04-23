18adaa0060705a601048d40a81f3b5b45362ee45
Daniel Edeen <115123138+dedeen@users.noreply.github.com>
2023-04-22 23:33:34 -0500

    Update vars.tf

--- terraform/vars.tf
+++ terraform/vars.tf
@@ -12,9 +12,12 @@ variable "app_vpcs" {
 			map_key			= 	"secvpc"
 			region_dc		= 	"Sec01-VPC"
 			cidr			= 	"10.100.0.0/16"
-			az_list			= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2b","us-west-2b","us-west-2b","us-west-2b"]
-			vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24","10.100.64.0/24","10.100.65.0/24","10.100.66.0/24","10.100.67.0/24"]
-			subnet_names	= 		["sec-az1-pub","sec-az1-priv","sec-az1-dmz","sec-az1-mgmt","sec-az2-pub","sec-az2-priv","sec-az2-dmz","sec-az2-mgmt"]
+			#az_list		= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a","us-west-2b","us-west-2b","us-west-2b","us-west-2b"]
+			#vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24","10.100.64.0/24","10.100.65.0/24","10.100.66.0/24","10.100.67.0/24"]
+			#subnet_names		= 	["sec-az1-pub","sec-az1-priv","sec-az1-dmz","sec-az1-mgmt","sec-az2-pub","sec-az2-priv","sec-az2-dmz","sec-az2-mgmt"]
+			az_list			= 	["us-west-2a","us-west-2a","us-west-2a","us-west-2a"]
+			vpc_subnets		= 	["10.100.0.0/24","10.100.1.0/24","10.100.2.0/24","10.100.3.0/24"]
+			subnet_names	= 		["sec-az1-pub","sec-az1-priv","sec-az1-dmz","sec-az1-mgmt"]
 		
 		}
 		
